ea9e9422db701ba2dbca980f28621383679b540b
Sam <muigai.samuel21@students.dkut.ac.ke>
2024-05-17 06:47:43 +0200

    Add retrofit and okhttp dependencies

--- app/src/main/java/com/samkt/projectmanagement/ui/SignUpFragment.java
+++ app/src/main/java/com/samkt/projectmanagement/ui/SignUpFragment.java
@@ -2,65 +2,66 @@ package com.samkt.projectmanagement.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-import com.samkt.projectmanagement.R;
+import com.samkt.projectmanagement.databinding.FragmentSignUpBinding;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SignUpFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class SignUpFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SignUpFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SignUpFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SignUpFragment newInstance(String param1, String param2) {
-        SignUpFragment fragment = new SignUpFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private FragmentSignUpBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        binding = FragmentSignUpBinding.inflate(inflater,container,false);
+        return binding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.btnSignUp.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (!isAllFieldsFilled()){
+                            Toast.makeText(requireContext(), "All the fields must be filled", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        if (!passwordMatches()){
+                            Toast.makeText(requireContext(), "Password and confirm password do not match", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        // TODO: 5/17/2024 Sign Up User
+                    }
+                }
+        );
+    }
+
+    private Boolean isAllFieldsFilled(){
+        return !binding.etConfirmPassword.getText().toString().isEmpty()
+                && !binding.etPassword.getText().toString().isEmpty()
+                && !binding.etUsername.getText().toString().isEmpty();
+    }
+
+    private Boolean passwordMatches(){
+        return binding.etPassword.getText().toString().equals(
+                binding.etConfirmPassword.getText().toString()
+        );
+    }
+
 }
